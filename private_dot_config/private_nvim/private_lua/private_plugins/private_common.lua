432dd7df638366d8f8a39456c29d387ceed4a168
Fryed Biscuit <fryedbiscuit@gmail.com>
2026-09-03 11:11:19 -0300

    update

--- private_dot_config/private_nvim/private_lua/private_plugins/private_common.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_common.lua
@@ -2,8 +2,7 @@ return {
 	{"tpope/vim-surround"},
 	{"tpope/vim-repeat"},
 	{"tpope/vim-speeddating"},
-	-- {"airblade/vim-gitgutter"},
-	
+	{"airblade/vim-gitgutter"},
 	{"justinmk/vim-sneak"},
 	{"LionC/nest.nvim"},
 	{"glts/vim-radical", dependencies = { "glts/vim-magnum" }},
